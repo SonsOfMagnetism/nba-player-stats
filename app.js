0637ab14a06a0a7a372c7fa68fc0f9d5c54eed89
Rene Santiago <rsj510@gmail.com>
2022-10-23 21:21:46 -0400

    rendered api data to screen

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://api-nba-v1.p.rapidapi.com/players?team=2&season=2022",
+	"url": "https://api-nba-v1.p.rapidapi.com/players?search=",
 	"method": "GET",
-    "timeout": 0,
 	"headers": {
-		"X-RapidAPI-Key": "a89584a87bmshd8602e41e99c3d1p190025jsnb1ab1d5587e1",
+		"X-RapidAPI-Key": "a2a63a7c59msha8758cc555ed553p136a91jsn192b7ec27de5",
 		"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
 	}
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+const $form = $("form")
+const $fName = $('#fName')
+
+
+function getStats(player) {
+	$.ajax(settings.url + player, settings)
+	.then((data) => {
+		for (let i = 0; i < data.response.length; i++) {
+			const search = data.response[i];
+			const playerId = data.response[i].id
+			const firstName = data.response[i].firstname
+			const lastName = data.response[i].lastname
+			if (search <= 1) {
+				$fName.append(`<span class="appended">Name: ${firstName} ${lastName}<br></span>`)
+			} else {
+				$fName.append(`<span class="appended">Name: ${firstName} ${lastName}</span>` + "<br>")
+			}
+
+			console.log(data)
+			console.log(playerId)
+		}
+
+
+	})
+}
+
+$form.submit((event) => {
+    event.preventDefault()
+    const textInput = $("input[type=text]").val()
+    getStats(textInput)
+})
